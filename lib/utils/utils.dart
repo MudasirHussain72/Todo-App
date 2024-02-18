be41ce5d6ca11be309a662bddfef9a004d46f6c7
MudasirHussain72 <mudasir7t1@gmail.com>
2024-02-18 15:58:54 +0500

    changes

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -43,7 +43,8 @@ class Utils {
       completionMessage = 'Congratulations!\nTask completed!';
       additionalMessage = 'Great job!';
     } else {
-      completionMessage = 'Invalid progress values';
+      // completionMessage = 'Invalid progress values';
+      completionMessage = '';
       additionalMessage = '';
     }
 
